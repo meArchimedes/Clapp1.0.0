5a93c2b5aafa8a4d5497fb55d637087e6c463d84
EAssaf <eassaf@spearhead-software.com>
2023-06-14 14:44:45 +0300

    address autocomplete half added, not working, scared to lose..

--- src/app/sign-up-forms/cleaner-sign-up/cleaner-su.component.ts
+++ src/app/sign-up-forms/cleaner-sign-up/cleaner-su.component.ts
@@ -1,31 +1,35 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { RouterExtensions } from "@nativescript/angular";
 import { File, knownFolders, path } from "@nativescript/core/file-system";
 import { NativeScriptFormsModule } from "@nativescript/angular";
 import { registerElement } from "@nativescript/angular";
-import { ListPicker } from "@nativescript/core";
-import { Http } from "@nativescript/core";
-import { openFile } from "@nativescript/core/utils";
-import { HandleFile } from "nativescript-handle-file";
+import { ScrollView } from "tns-core-modules";
+import { TextField } from "@nativescript/core";
+import { GooglePlacesAutocomplete } from "nativescript-google-places-autocomplete";
 import * as FilePicker from "@prabudevarrajan/filepicker";
-// import * as FilePicker  from 'nativescript-plugin-filepicker';
+import { HttpClient } from "@angular/common/http";
 
-// registerElement("ListPicker", () => ListPicker);
+
+
 @Component({
   selector: "cleaner-su",
   templateUrl: "cleaner-su.component.html",
   styleUrls: ["cleaner-su.component.scss"],
-  moduleId: module.id,
+  moduleId: module.id
 })
 export class CleanerSignUpComponent implements OnInit {
-  name: string;
-  id: string;
-  workPermitFile: any;
+  placesAutocomplete: any;
+  autocompleteResults: any[] = [];
+  isSignUpEnabled: boolean = false;
+  name: string = "";
+  id: string = "";
+  workPermitFile: any = null;
   proofOfIdFile: any;
-  address: string;
-  gender: string;
-  email: string;
-  password: string;
+  address: string = "";
+  gender: string = "";
+  email: string = "";
+  password: string = "";
+  confirmedPassword: string = "";
 
   dropdownOptions: Array<string> = ["Female", "Male", "Rather not say"];
   selectedOption: string;
@@ -35,8 +39,14 @@ export class CleanerSignUpComponent implements OnInit {
   //   this.selectedFile = files.item(0);
   // }
 
-  constructor(private router: RouterExtensions) {}
-  ngOnInit() {}
+  constructor(private router: RouterExtensions, private http: HttpClient) {}
+  ngOnInit() {
+    this.onSignUpEnabled();
+    this.placesAutocomplete = new GooglePlacesAutocomplete(
+      "AIzaSyArDTr6RHhG1z3AZxR8uQCKem7eXbXn-ow"
+    );
+  }
+
   signUp() {
     // Handle the sign-up logic here
     // You can access the form values stored in the component properties (e.g., this.name, this.email)
@@ -47,6 +57,21 @@ export class CleanerSignUpComponent implements OnInit {
     // Redirect to the desired page after successful sign-up
     this.router.navigate(["/calendars/calendar/calendar-tabs"]);
   }
+  onAddressInputChange(args) {
+    let textField = <TextField>args.object;
+    console.log(textField.text);
+
+    this.placesAutocomplete.search(textField.text).then(
+      (places: any) => {
+        console.log(places);
+        // place predictions list
+      },
+      error => {
+        throw error;
+      }
+    );
+  }
+
   uploadFile(): void {
     // Perform the file upload logic here using this.selectedFile
     if (this.selectedFile) {
@@ -54,24 +79,44 @@ export class CleanerSignUpComponent implements OnInit {
       // Additional logic...
     }
   }
-  onSignUpEnabled() {}
+  onSignUpEnabled() {
+    // Check if all required fields are filled
+    if (
+      this.name !== "" &&
+      this.email !== "" &&
+      this.workPermitFile !== "" &&
+      this.proofOfIdFile !== "" &&
+      this.address !== "" &&
+      this.gender !== "" &&
+      this.password !== ""
+    ) {
+      this.isSignUpEnabled = true;
+    } else {
+      this.isSignUpEnabled = false;
+    }
+  }
+  onPredictionSelected(selectedIndex: number) {
+    const selectedPrediction = this.autocompleteResults[selectedIndex];
+    console.log("Selected Prediction:", selectedPrediction);
+    this.address = selectedPrediction.description;
+  }
 
   onSelectFile() {
     let context = FilePicker.create({
       mode: "single",
-      extensions: ["pdf", "jpg", "doc", "docx"],
+      extensions: ["pdf", "jpg", "doc", "docx"]
     });
     context
       .authorize()
-      .then(function () {
+      .then(function() {
         return context.present();
       })
-      .then(function (selection) {
-        selection.forEach(function (selected) {
+      .then(function(selection) {
+        selection.forEach(function(selected) {
           // process the selected file
         });
       })
-      .catch(function (e) {
+      .catch(function(e) {
         // process error
       });
   }
